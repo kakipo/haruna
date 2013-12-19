cdfa2bf392923ef592b4a75d7f2bd70f782d02d8
内藤 研介 <kakipo@iMac.local>
2013-12-20 08:44:25 +0900

    added .ruby-version

--- lib/haruna.rb
+++ lib/haruna.rb
@@ -3,4 +3,13 @@ require "haruna/extention/object"
 
 module Haruna
   # Your code goes here...
+  class Client
+    def initialize(token)
+      @token = token
+    end
+
+    def say
+      p @token
+    end
+  end
 end
